7908e00b3ea74be0d9d75acf7a6cd2d1734bd84d
CreatorsDevs <creatorsdevs@gmail.com>
2023-11-08 02:01:23 +0530

    Added mechanism for handling direction changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,18 +23,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moving sideways
         rb.velocity = new Vector2 (Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
 
+        // Handle direction changes
+        if(rb.velocity.x < 0 )
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(rb.velocity.x > 0 )
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        // Checking if player is on ground
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
 
+        // Jumping
         if(Input.GetButtonDown("Jump") && isOnGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-
-
-
+        // Animation controllers
         anim.SetBool("isOnGround", isOnGround);
         anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
     }
