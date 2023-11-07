998bd0b8b46456b2fb84e564f657fc062d05ae70
CreatorsDevs <creatorsdevs@gmail.com>
2023-11-07 20:54:27 +0530

    Added Jumping Mechanism

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Transform groundPoint;
+
+    private bool isOnGround;
+    public LayerMask whatIsGround;
 
     void Start()
     {
@@ -16,5 +22,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2 (Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
+
+        if(Input.GetButtonDown("Jump") && isOnGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
