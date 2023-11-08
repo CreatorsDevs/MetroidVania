b57ffdc0c5a56b61e80b43fc4345518cd4965d07
CreatorsDevs <creatorsdevs@gmail.com>
2023-11-08 15:24:59 +0530

    Added camera clamping effect

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D cameraBounds;
+
     private PlayerController player;
+    private float halfHeight, halfWidth;
+    
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
     }
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(
+            Mathf.Clamp( player.transform.position.x, cameraBounds.bounds.min.x + halfWidth, cameraBounds.bounds.max.x - halfWidth),
+            Mathf.Clamp(player.transform.position.y, cameraBounds.bounds.min.y + halfHeight, cameraBounds.bounds.max.y - halfHeight), 
+            transform.position.z);
     }
 }
