7327eea43a9c2f1ad9e4138f363668c721c2f810
CreatorsDevs <creatorsdevs@gmail.com>
2023-11-09 17:15:35 +0530

    Added Impact Effect

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,6 +6,7 @@ public class BulletController : MonoBehaviour
 {
     [SerializeField] private float bulletSpeed;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject impactEffect;
 
     public Vector2 moveDir;
     // Start is called before the first frame update
@@ -22,6 +23,10 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(impactEffect != null)
+        {
+            Instantiate(impactEffect, transform.position, Quaternion.identity);
+        }
         Destroy(gameObject);
     }
 
