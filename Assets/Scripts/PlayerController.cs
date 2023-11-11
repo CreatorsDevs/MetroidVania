544716f1abda271c3bb98dc12dcf7a46067ba51b
CreatorsDevs <creatorsdevs@gmail.com>
2023-11-11 14:30:22 +0530

    Added Player Dash Ability

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform groundPoint;
     [SerializeField] private BulletController shotToFire;
     [SerializeField] private Transform shotPoint;
+    [SerializeField] private float dashSpeed, dashTime;
 
     
     public LayerMask whatIsGround;
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour
 
     private bool isOnGround;
     private bool canDoubleJump;
-
+    private float dashCounter;
 
     void Start()
     {
@@ -24,17 +25,30 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Moving sideways
-        rb.velocity = new Vector2 (Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
-
-        // Handle direction changes
-        if(rb.velocity.x < 0 )
+        if(Input.GetButtonDown("Fire2"))
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            dashCounter = dashTime;
         }
-        else if(rb.velocity.x > 0 )
+
+        if (dashCounter > 0)
         {
-            transform.localScale = Vector3.one;
+            dashCounter = dashCounter - Time.deltaTime;
+            rb.velocity = new Vector2(dashSpeed * transform.localScale.x, rb.velocity.y);
+        }
+        else
+        {
+            // Moving sideways
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+
+            // Handle direction changes
+            if (rb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (rb.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
         }
 
         // Checking if player is on ground
