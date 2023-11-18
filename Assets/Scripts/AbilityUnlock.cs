8bb3911e0416328dade58c8b5120397afb8b5949
CreatorsDevs <creatorsdevs@gmail.com>
2023-11-18 19:31:38 +0530

    Added text effects for abilities pickups

--- Assets/Scripts/AbilityUnlock.cs
+++ Assets/Scripts/AbilityUnlock.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AbilityUnlock : MonoBehaviour
 {
     public bool unlockDoubleJump, unlockDash, unlockBecomeBall, unlockDropBomb;
+
+    public GameObject pickupEffect;
+
+    public string unlockMessage;
+    public TMP_Text unlockText;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -32,6 +38,14 @@ public class AbilityUnlock : MonoBehaviour
                 player.canDropBomb = true;
             }
 
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+
+            unlockText.transform.parent.SetParent(null);
+            unlockText.transform.parent.position = transform.position;
+            unlockText.text = unlockMessage;
+            unlockText.gameObject.SetActive(true);
+
+            Destroy(unlockText.transform.parent.gameObject, 5f);
             Destroy(gameObject);
         }
     }
